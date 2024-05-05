0784307fc0d2935650930a3007d70ca322d67971
Omar Elweshy <omarelweshy@gmail.com>
2024-05-06 00:45:06 +0300

    CORS

--- backend/src/main/java/com/googledocs/googledocsbackend/GoogleDocsBackendApplication.java
+++ backend/src/main/java/com/googledocs/googledocsbackend/GoogleDocsBackendApplication.java
@@ -2,6 +2,10 @@ package com.googledocs.googledocsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class GoogleDocsBackendApplication {
@@ -10,4 +14,14 @@ public class GoogleDocsBackendApplication {
 		SpringApplication.run(GoogleDocsBackendApplication.class, args);
 	}
 
+    @Configuration
+    public class CorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*");
+        }
+    }
 }
--- backend/src/main/java/com/googledocs/googledocsbackend/controller/UserController.java
+++ backend/src/main/java/com/googledocs/googledocsbackend/controller/UserController.java
@@ -12,38 +12,24 @@ import java.util.*;
 
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    private static class TokenResponse {
-        private final String id;
-        private final String token;
-
-        public TokenResponse(String id, String token) {
-            this.id = id;
-            this.token = token;
-        }
-    }
-
     @PostMapping("/signup")
     public ResponseEntity<Object> createUser(@RequestBody User user) {
         Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
         if (existingUser.isPresent()) {
-            // User with the same email already exists, return it
             return new ResponseEntity<>(existingUser.get(), HttpStatus.OK);
         } else {
-            // User with the email does not exist, create a new user
             User savedUser = userRepository.save(user);
             String token = JwtUtil.generateToken(user.getEmail());
             Map<String, String> data = new HashMap<>();
             data.put("token", token);
             data.put("id", savedUser.getId());
             return new ResponseEntity<>(data, HttpStatus.OK);
-
-            // return ResponseEntity.status(HttpStatus.CREATED)
         }
     }
 
